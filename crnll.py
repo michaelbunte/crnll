728aa543713c2176e590ba0a899cd16d815a6e30
Michael Bunte <michaelwbunte@gmail.com>
2024-07-06 11:46:40 -0700

    forwards seek

--- crnll.py
+++ crnll.py
@@ -1,6 +1,8 @@
 import argparse
 import subprocess
 import os
+import getch
+
 
 
 parser = argparse.ArgumentParser(
@@ -20,28 +22,48 @@ SPAS = {
 notes_filename = args.file
 questions_filename = args.questions if args.questions != None else "q_" + notes_filename
 
-with open(questions_filename, "r") as f:
+
+def seek_forwards(f, seek_stack):
+    f.seek(seek_stack[-1])
+    os.system("clear")
+    print()
+    print(f.readline(), end="")
+    print()
+
     while True:
-        input("")
-        os.system("clear")
-        print()
-        print(f.readline(), end="")
-        print()
+        pos = f.tell()
 
-        while True:
-            pos = f.tell()
+        subprocess.run("touch eco.txt", **SPAS)
+        g = open("eco.txt", "w")
+        eco = f.readline()
+        g.write(eco)
+        g.close()
+        if(int(subprocess.run("cat eco.txt | grep -E '^\d+\-?\d*\.' | wc -c | tr -d ' '", **SPAS).stdout) != 0):
+            f.seek(pos)
+            print(os.get_terminal_size()[0] * "=")
+            seek_stack.append(f.tell())
+            break
+        print(eco, end="")
 
-            
-            subprocess.run("touch eco.txt", **SPAS)
-            g = open("eco.txt", "w")
-            eco = f.readline()
-            g.write(eco)
-            g.close()
-            if(int(subprocess.run("cat eco.txt | grep -E '^\d+\-?\d*\.' | wc -c | tr -d ' '", **SPAS).stdout) != 0):
-                f.seek(pos)
-                print(os.get_terminal_size()[0] * "=")
-                break
-            
+def seek_backwards(f, seek_stack):
+    len(seek_stack) > 1 and seek_stack.pop()
+    len(seek_stack) > 1 and seek_stack.pop()
+    print(len(seek_stack))
 
-            print(eco, end="")
+    seek_forwards(f, seek_stack)
+
+
+
+with open(questions_filename, "r") as f:
+    seek_stack = [f.tell()]
+
+    while True:
+        f.seek(seek_stack[-1])
+        char = getch.getch()
+        if char == "l":
+            seek_forwards(f, seek_stack)
+        if char == "j":
+            seek_backwards(f, seek_stack)
+
+
 
